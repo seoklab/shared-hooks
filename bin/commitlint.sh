0dc4a6e52f5a53995835b900d187211ac317e59a
jnooree <jnooree@snu.ac.kr>
2023-04-17 10:23:01 +0900

    feat: create hook for commitlint

---
+++ bin/commitlint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# commitlint.sh - Shared Hooks
+#
+# Copyright (c) 2023 SNU Compbio Lab.
+#
+# SPDX-License-Identifier: MIT
+#
+
+if ! CL_BIN="$(type -P commitlint)"; then
+	if [[ -n "$HOMEBREW_PREFIX" ]]; then
+		CL_BIN="$HOMEBREW_PREFIX/bin/commitlint"
+	else
+		case "$(uname -s)" in
+		Darwin)
+			case "$(uname -m)" in
+			x86_64)
+				CL_BIN="/usr/local/bin/commitlint"
+				;;
+			arm64)
+				CL_BIN="/opt/homebrew/bin/commitlint"
+				;;
+			*)
+				echo "ERROR: Unsupported architecture."
+				exit 1
+				;;
+			esac
+			;;
+		Linux)
+			CL_BIN="/home/linuxbrew/.linuxbrew/bin/commitlint"
+			;;
+		*)
+			echo "ERROR: Unsupported OS"
+			exit 1
+			;;
+		esac
+	fi
+fi
+
+if [[ ! -x "$CL_BIN" ]]; then
+	echo "ERROR: commitlint not found or not executable."
+	exit 1
+fi
+
+sed '/# ------------------------ >8 ------------------------/q' "$1" |
+	grep -v '^#' |
+	exec "$CL_BIN" -V
