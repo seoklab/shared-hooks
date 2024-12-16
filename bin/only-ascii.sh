92a22e4b091e976c781115e182f92658994d1a14
Nuri Jung <jnooree@snu.ac.kr>
2024-12-16 14:06:51 +0900

    feat: add ascii-only pre-commit check

---
+++ bin/only-ascii.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+non_ascii="$(
+	git diff HEAD -- "$@" |
+		grep '^+[^+]' |
+		tr -d "\011\012\015\040-\176"
+)"
+
+if [[ -z $non_ascii ]]; then
+	exit 0
+fi
+
+echo "ERROR: Non-ASCII characters detected in the diff. \
+Please remove them before committing:
+
+$non_ascii"
+exit 1
